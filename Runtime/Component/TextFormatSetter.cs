42f8a728870cf39f269998a2363d254c1d9125c9
이성희 <sungheelee@com2us.com>
2025-07-09 16:14:35 +0900

    TextMaterialChanger 상속 가능하게 수정

--- Runtime/Component/TextFormatSetter.cs
+++ Runtime/Component/TextFormatSetter.cs
@@ -56,21 +56,24 @@ namespace UnityEngine.UI
         private object[] _params;
 
 
-        private void Awake()
+        protected virtual void Awake()
         {
             m_Text = GetComponent<Text>();
             m_TMPText = GetComponent<TMPro.TMP_Text>();
         }
 
-        public void SetParameter(params object[] p)
+        public virtual void SetParameter(params object[] p)
         {
             _params = p;
             UpdateText();
         }
 
-        private void UpdateText()
+        protected virtual string GetFormatString() => format;
+
+        protected virtual void UpdateText()
         {
             string str = null;
+            string formatStr = GetFormatString();
 
 #if UNITY_EDITOR
             if (Application.isPlaying == false)
@@ -78,18 +81,18 @@ namespace UnityEngine.UI
                 try
                 {
                     if (string.IsNullOrWhiteSpace(m_TestParam3) == false)
-                        str = string.Format(format, m_TestParam1, m_TestParam2, m_TestParam3);
+                        str = string.Format(formatStr, m_TestParam1, m_TestParam2, m_TestParam3);
                     else if (string.IsNullOrWhiteSpace(m_TestParam2) == false)
-                        str = string.Format(format, m_TestParam1, m_TestParam2);
+                        str = string.Format(formatStr, m_TestParam1, m_TestParam2);
                     else if (string.IsNullOrWhiteSpace(m_TestParam1) == false)
-                        str = string.Format(format, m_TestParam1);
+                        str = string.Format(formatStr, m_TestParam1);
                     else
-                        str = format;
+                        str = formatStr;
                 }
                 catch (System.Exception e)
                 {
                     Debug.LogException(e);
-                    str = format;
+                    str = formatStr;
                 }
             }
             else
@@ -102,35 +105,35 @@ namespace UnityEngine.UI
                         try
                         {
                             if (_params.Length >= 3)
-                                str = string.Format(format, _params[0], _params[1], _params[2]);
+                                str = string.Format(formatStr, _params[0], _params[1], _params[2]);
                             else if (_params.Length >= 2)
-                                str = string.Format(format, _params[0], _params[1]);
+                                str = string.Format(formatStr, _params[0], _params[1]);
                             else if (_params.Length >= 1)
-                                str = string.Format(format, _params[0]);
+                                str = string.Format(formatStr, _params[0]);
                             else
-                                str = format;
+                                str = formatStr;
                         }
                         catch (System.Exception e)
                         {
                             Debug.LogException(e);
-                            str = format;
+                            str = formatStr;
                         }
                     }
                     else
                     {
                         if (_params.Length >= 3)
-                            str = string.Format(format, _params[0], _params[1], _params[2]);
+                            str = string.Format(formatStr, _params[0], _params[1], _params[2]);
                         else if (_params.Length >= 2)
-                            str = string.Format(format, _params[0], _params[1]);
+                            str = string.Format(formatStr, _params[0], _params[1]);
                         else if (_params.Length >= 1)
-                            str = string.Format(format, _params[0]);
+                            str = string.Format(formatStr, _params[0]);
                         else
-                            str = format;
+                            str = formatStr;
                     }
                 }
                 else
                 {
-                    str = format;
+                    str = formatStr;
                 }
             }
 
@@ -138,12 +141,12 @@ namespace UnityEngine.UI
             if (tmpText != null) tmpText.text = str;
         }
 
-        private void OnValidate()
+        protected virtual void OnValidate()
         {
             UpdateText();
         }
 
-        private void Reset()
+        protected virtual void Reset()
         {
             if (text != null) m_Format = text.text;
             else if (tmpText != null) m_Format = tmpText.text;
